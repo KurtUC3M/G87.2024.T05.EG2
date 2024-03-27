f8754f6a8b14641fc488a8cdcf9e7ba5da532346
Liang Ji Zhu <www.1159068228@gmail.com>
2024-03-27 23:38:42 +0100

    requirement2

---
+++ src/unittest/python/test_arrival_hotels_tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+from UC3MTravel.HotelManager import HotelManager
+from UC3MTravel.HotelManagementException import HotelManagementException
+
+class TestArrivalHotel(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
